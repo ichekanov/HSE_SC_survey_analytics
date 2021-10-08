19f1753476b9fcec280aeabd33a6c5d901cff54c
olskalim <is.2003.01@gmail.com>
2021-10-08 23:00:50 +0300

    Update getData.py

--- parser script/getData.py
+++ parser script/getData.py
@@ -1,16 +1,22 @@
 import vk
 from private_data import VK_personal_token
 
-session = vk.Session(access_token=VK_personal_token)
-api = vk.API(session, v='5.131', lang='ru')
 
-poll_id = "637266255"
+def get_poll_results(VK_access_token, poll_id):
+    session = vk.Session(access_token=VK_access_token)
+    api = vk.API(session, v='5.131', lang='ru')
+    poll_data = api.polls.getById(poll_id=poll_id)
+    variants = [x['id'] for x in poll_data['answers']]
+    variants_with_names = [(x['id'], x['text']) for x in poll_data['answers']]
+    votes = api.polls.getVoters(poll_id=poll_id, answer_ids=variants)
+    users_by_variants = {}
+    for k in range(len(variants_with_names)):
+        users_by_variants[variants_with_names[k]] = votes[k]['users']['items']
+    return users_by_variants  # {(variant_id, variant_name): [voters], ...}
 
-pollData = api.polls.getById(poll_id=poll_id)
-variants = [x['id'] for x in pollData['answers']]
-votes = api.polls.getVoters(poll_id=poll_id, answer_ids=variants)
-usersByVariants = {x['answer_id']: x['users']['items'] for x in votes}
 
-for k in usersByVariants:
-    print(k, usersByVariants[k], sep=': ', end='\n')
+if __name__ == "__main__":
+    poll_id = "637266255"
+    print(get_poll_results(VK_personal_token, poll_id))
+
 # script output in output.txt
