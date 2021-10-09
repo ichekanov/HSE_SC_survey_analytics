2855a71ca5a72142216d2def38647daa010c682f
olskalim <is.2003.01@gmail.com>
2021-10-09 23:00:25 +0300

    Done!

--- parser script/getData.py
+++ parser script/getData.py
@@ -1,13 +1,16 @@
 import csv
 import requests
+from time import sleep
 import vk
 from private_data import VK_personal_token
+
 
 def get_polls(owner_id):
     ID = []
     k = 0
     url = f"https://api.vk.com/method/wall.get?owner_id={owner_id}&access_token={VK_personal_token}&v=5.131"
-    req = requests.get(url, params={'owner_id': owner_id,'count': 100, 'offset': k})
+    req = requests.get(
+        url, params={'owner_id': owner_id, 'count': 100, 'offset': k})
     src = req.json()
     mx = src["response"]["count"]
     while k < mx:
@@ -18,9 +21,10 @@ def get_polls(owner_id):
             if "attachments" in post:
                 for x in post["attachments"]:
                     if "poll" in x:
-                        ID.append(x["poll"]["id"])
+                        ID.append((x["poll"]["id"], x["poll"]["question"]))
             k += 1
     return ID  # [id, ...]
+
 
 def get_poll_results(poll_id):
     poll_data = api.polls.getById(poll_id=poll_id)
@@ -40,35 +44,57 @@ def get_names(ids):
     return names  # [(last_name, first_name, id), ...]
 
 
-def write_to_csv(names, results, answers, path="output.csv"):
-    ids = [x[2] for x in names]
-    with open(path, "w", newline='') as csv_file:
+def write_to_csv(data, path="data/output.csv"):
+    ids = [x[2] for x in voters]
+    with open(path, "w", newline='', encoding='utf-8') as csv_file:
         writer = csv.writer(csv_file, delimiter=',')
-        writer.writerow(['']+[x[0]+" "+x[1] for x in names])
-        for i, k in enumerate(answers):
-            row = [k[1]]
-            row += [1 if x in results[i] else 0 for x in ids]
+        writer.writerow(['', ''] + [x[0]+" "+x[1] for x in voters])
+        for k in data:
+            # k = ((poll_id, poll_name), (var_id, var_name)): [voters]
+            row = [k[0][1], k[1][1]]
+            row += [1 if x in data[k] else 0 for x in ids]
             writer.writerow(row)
 
+# def compose_by_one(group_id):
+#     polls = get_polls(group_id)
+#     for poll_id in polls:
+#         results = list(get_poll_results(poll_id).values())
+#         answers = list(get_poll_results(poll_id))
+#         voters_ids = set()
+#         for i in range(len(results)):
+#             for x in results[i]:
+#                 voters_ids.add(x)
+#         voters = sorted(get_names(list(voters_ids)))
+#         write_to_csv(voters, results, answers, f"data/poll-{poll_id}.csv")
+
+
+def compose_all(group_id):
+    polls = get_polls(group_id)  # (poll_id, poll_name)
+    voters_ids = set()  # IDs of voters
+    table = dict()
+    for poll in polls:
+        # ID for each variant
+        answers = get_poll_results(poll[0])
+        results = list(answers.values())
+        answers = list(answers)  # IDs and names of variants
+        for i in range(len(results)):
+            for x in results[i]:
+                voters_ids.add(x)  # adding voters
+        for i, var in enumerate(answers):
+            table[(poll, var)] = results[i]
+        sleep(1)
+    global voters
+    voters = sorted(get_names(list(voters_ids)))  # names and IDs of voters
+    return table  # {((poll_id, poll_name), (var_id, var_name)): [voters], ...}
 
 
 def main():
-    # poll_id = "637266255"
-    group_id = "-206802048" 
-    polls = get_polls(group_id)
-    for poll_id in polls:
-        results = list(get_poll_results(poll_id).values())
-        answers = list(get_poll_results(poll_id))
-        voters_ids = set()
-        for i in range(len(results)):
-            for x in results[i]:
-                voters_ids.add(x)
-        voters = sorted(get_names(list(voters_ids)))
-        write_to_csv(voters, results, answers, f"data/poll-{poll_id}.csv")
+    group_id = "-206802048"
+    write_to_csv(compose_all(group_id))
+
 
 if __name__ == "__main__":
     session = vk.Session(access_token=VK_personal_token)
     api = vk.API(session, v='5.131', lang='ru')
+    voters = []
     main()
-
-# script output in output.txt
