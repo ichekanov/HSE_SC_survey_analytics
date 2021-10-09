b1130e0aac33484f0d6ad093d7a2c3b3a05607ba
olskalim <is.2003.01@gmail.com>
2021-10-09 21:05:11 +0300

    Release
    We can get separated csv reports for each poll on the wall
    
    Co-Authored-By: Dima191 <67740180+dima191@users.noreply.github.com>

--- parser script/getData.py
+++ parser script/getData.py
@@ -1,7 +1,26 @@
 import csv
+import requests
 import vk
 from private_data import VK_personal_token
 
+def get_polls(owner_id):
+    ID = []
+    k = 0
+    url = f"https://api.vk.com/method/wall.get?owner_id={owner_id}&access_token={VK_personal_token}&v=5.131"
+    req = requests.get(url, params={'owner_id': owner_id,'count': 100, 'offset': k})
+    src = req.json()
+    mx = src["response"]["count"]
+    while k < mx:
+        req = requests.get(url, params={'owner_id': owner_id, 'offset': k})
+        src = req.json()
+        posts = src["response"]["items"]
+        for post in posts:
+            if "attachments" in post:
+                for x in post["attachments"]:
+                    if "poll" in x:
+                        ID.append(x["poll"]["id"])
+            k += 1
+    return ID  # [id, ...]
 
 def get_poll_results(poll_id):
     poll_data = api.polls.getById(poll_id=poll_id)
@@ -21,26 +40,31 @@ def get_names(ids):
     return names  # [(last_name, first_name, id), ...]
 
 
-def write_to_csv(names, users_by_variants, path="output.csv"):
-    ids = [x[3] for x in names]
+def write_to_csv(names, results, answers, path="output.csv"):
+    ids = [x[2] for x in names]
     with open(path, "w", newline='') as csv_file:
         writer = csv.writer(csv_file, delimiter=',')
         writer.writerow(['']+[x[0]+" "+x[1] for x in names])
+        for i, k in enumerate(answers):
+            row = [k[1]]
+            row += [1 if x in results[i] else 0 for x in ids]
+            writer.writerow(row)
+
 
 
 def main():
-    poll_id = "637266255"
-    results = list(get_poll_results(poll_id).values())
-    answers = list(get_poll_results(poll_id))
-    voters_ids = set()
-    for i in range(len(results)):
-        for x in results[i]:
-            voters_ids.add(x)
-    voters = sorted(get_names(list(voters_ids)))
-    # print(voters)
-    print(answers)
-    # write_to_csv(voters, results)
-
+    # poll_id = "637266255"
+    group_id = "-206802048" 
+    polls = get_polls(group_id)
+    for poll_id in polls:
+        results = list(get_poll_results(poll_id).values())
+        answers = list(get_poll_results(poll_id))
+        voters_ids = set()
+        for i in range(len(results)):
+            for x in results[i]:
+                voters_ids.add(x)
+        voters = sorted(get_names(list(voters_ids)))
+        write_to_csv(voters, results, answers, f"data/poll-{poll_id}.csv")
 
 if __name__ == "__main__":
     session = vk.Session(access_token=VK_personal_token)
