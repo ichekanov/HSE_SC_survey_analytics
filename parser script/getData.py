85254f72ce634ce9588992d5ffe9f6c7aed95b41
olskalim <is.2003.01@gmail.com>
2021-10-09 19:25:35 +0300

    Update getData.py

--- parser script/getData.py
+++ parser script/getData.py
@@ -1,10 +1,9 @@
+import csv
 import vk
 from private_data import VK_personal_token
 
 
-def get_poll_results(VK_access_token, poll_id):
-    session = vk.Session(access_token=VK_access_token)
-    api = vk.API(session, v='5.131', lang='ru')
+def get_poll_results(poll_id):
     poll_data = api.polls.getById(poll_id=poll_id)
     variants = [x['id'] for x in poll_data['answers']]
     variants_with_names = [(x['id'], x['text']) for x in poll_data['answers']]
@@ -15,8 +14,37 @@ def get_poll_results(VK_access_token, poll_id):
     return users_by_variants  # {(variant_id, variant_name): [voters], ...}
 
 
-if __name__ == "__main__":
+def get_names(ids):
+    # max number of requested users - 1000
+    names = sorted([(x['last_name'], x['first_name'], x['id'])
+                    for x in api.users.get(user_ids=ids)])
+    return names  # [(last_name, first_name, id), ...]
+
+
+def write_to_csv(names, users_by_variants, path="output.csv"):
+    ids = [x[3] for x in names]
+    with open(path, "w", newline='') as csv_file:
+        writer = csv.writer(csv_file, delimiter=',')
+        writer.writerow(['']+[x[0]+" "+x[1] for x in names])
+
+
+def main():
     poll_id = "637266255"
-    print(get_poll_results(VK_personal_token, poll_id))
+    results = list(get_poll_results(poll_id).values())
+    answers = list(get_poll_results(poll_id))
+    voters_ids = set()
+    for i in range(len(results)):
+        for x in results[i]:
+            voters_ids.add(x)
+    voters = sorted(get_names(list(voters_ids)))
+    # print(voters)
+    print(answers)
+    # write_to_csv(voters, results)
+
+
+if __name__ == "__main__":
+    session = vk.Session(access_token=VK_personal_token)
+    api = vk.API(session, v='5.131', lang='ru')
+    main()
 
 # script output in output.txt
